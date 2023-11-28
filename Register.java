2e7ac46f767ec1698781e013b0e7df5591a015b3
NethmiRajapaksha <117499065+NethmiRajapaksha@users.noreply.github.com>
2023-11-28 23:11:43 +0530

    Add files via upload

---
+++ Register.java
@@ -0,0 +1,84 @@
+package com.example.smilingheart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
+
+import static android.hardware.Sensor.TYPE_LIGHT;
+
+public class
+Register extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private Sensor lightSensor;
+    private SensorEventListener lightEventListener;
+    private View nethmi;
+    private float maxVlue;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register2);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(Register.this, Welcome.class);
+                startActivity(i);
+                finish();
+            }
+        },1000000);
+        nethmi = findViewById(R.id.nethmi);
+
+        //Light sensor implementation
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        lightSensor = sensorManager.getDefaultSensor(TYPE_LIGHT);
+
+        if (lightSensor == null) {
+            Toast.makeText(this, "This device has no light sensor :(", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        maxVlue = lightSensor.getMaximumRange();
+        lightEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                float value = sensorEvent.values[0];
+                int newValue = (int) (255f * value / maxVlue);
+                nethmi.setBackgroundColor(Color.rgb(newValue, newValue, newValue));
+            }
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(lightEventListener, lightSensor, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(lightEventListener);
+    }
+
+    //Connect pages using buttons
+    public void signupButton(View view){
+        Intent intent = new Intent(this,Donor.class);
+        startActivity(intent);
+    }
+    public void loginBtn(View view){
+        Intent intent = new Intent(this,LogIn.class);
+        startActivity(intent);
+    }
+}
